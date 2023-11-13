bc79a6cc17354f80eb120fb4dd3c57ef04d6fd2b
Gendralf <sazhinalex2003@gmail.com>
2023-11-13 14:49:52 +0300

    Шаблоны для окон вставки

---
+++ windows/other_windows/addclassroomwidget.py
@@ -0,0 +1,60 @@
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QPushButton, QLineEdit, QHBoxLayout
+
+
+# Создание окна для аудитории
+
+class AddingClassroomWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.massive = []
+        self.layout = QVBoxLayout()
+        self.layout.setAlignment(Qt.AlignTop)
+        self.setLayout(self.layout)
+        self.addLine()
+
+    def addLine(self):
+        line = QWidget()
+        line.plus_button = QPushButton("+")
+        line.classroom = QLineEdit()
+        line.classroom.setMaximumWidth(150)
+
+        line.size = QLineEdit()
+        line.size.setFixedWidth(50)
+        line.cancel_button = QPushButton("X")
+
+        line.plus_button.clicked.connect(self.addLine)
+        line.cancel_button.clicked.connect(lambda: self.delLine(self.massive.index(line)))
+
+        line_layout = QHBoxLayout()
+        line_layout.addWidget(line.plus_button)
+        line_layout.addWidget(line.classroom)
+        line_layout.addWidget(line.size)
+        line_layout.addWidget(line.cancel_button)
+        line.setLayout(line_layout)
+
+        self.massive.append(line)
+        self.updateLayout()
+
+    def delLine(self, num_in_list):
+        self.massive.pop(num_in_list)
+        self.updateLayout()
+
+    def clearLayout(self):
+        for i in reversed(range(self.layout.count())):
+            self.layout.itemAt(i).widget().setParent(None)
+
+    def updateLayout(self):
+        self.clearLayout()
+        if len(self.massive) == 1:
+            self.massive[0].cancel_button.setVisible(False)
+            self.massive[0].plus_button.setVisible(True)
+            self.layout.addWidget(self.massive[0])
+        else:
+            for index in range(len(self.massive)):
+                if index < len(self.massive) - 1:
+                    self.massive[index].plus_button.setVisible(False)
+                else:
+                    self.massive[index].plus_button.setVisible(True)
+                self.massive[index].cancel_button.setVisible(True)
+                self.layout.addWidget(self.massive[index])
