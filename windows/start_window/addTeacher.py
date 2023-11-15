da79345f53f952632f58ab791385d241e4694b6e
mortalboy <zhenya-2003@bk.ru>
2023-11-16 00:03:12 +0300

    Добавил окна для классов добавления(надо ждать когда пределают классы)

---
+++ windows/start_window/addTeacher.py
@@ -0,0 +1,107 @@
+from pathlib import Path
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+from PySide6.QtWidgets import *
+
+from windows.other_windows.addteacherwidget import AddingTeacherWidget
+
+class AddTeacher(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # Настройка окна
+        self.setMinimumSize(900, 800)
+        self.setWindowTitle("OOO Knopocnie Kabanchiki 3C++")
+
+        # Используемые виджеты
+        # 1) Текст сверху
+        # 2) Виджет заполнения (плюс размер окна заполнения)
+        # 3) Кнопки далее/назад
+
+        # label -> Виджет добавления -> Кнопки
+        # 1)
+
+        # Виджет нужен для того, чтобы поделить экран
+        widget_label = QWidget()
+
+        # Создаём layout
+        widget_label_layout = QHBoxLayout()
+
+        # Запихиваем layout наверх
+        widget_label_layout.setAlignment(Qt.AlignTop)
+        # Создаём label
+        label = QLabel("Введите преподавателей")
+
+        # Центрую label
+        label.setAlignment(Qt.AlignCenter)
+
+        # Запихиваем в layout label
+        widget_label_layout.addWidget(label)
+
+        # Задаём виджету layout
+        widget_label.setLayout(widget_label_layout)
+
+        # 2)
+
+        # В патерн записываем функция добавления
+        pattern = AddingTeacherWidget()
+
+        # Добавляем скрол бар
+        widget_add = QScrollArea()
+        # ?
+        widget_add.setWidget(pattern)
+        widget_add.setWidgetResizable(True)
+
+        widget_add.setMinimumSize(500, 500)
+
+        # 3)
+        widget_button = QWidget()
+
+        # Создание мэйнового виджета в который запихиваем 3 виджета (делим всё окно на 3 области) и сразу же layout
+        main_widget = QWidget()
+        main_layout = QVBoxLayout()
+
+        # Создаю кнопки и задаю размер
+        # Кнопка назад
+        button_back = QPushButton("Назад")
+        button_back.setFixedSize(120, 50)
+        # Кнопка далее
+        button_next = QPushButton("Далее")
+        button_next.setFixedSize(120, 50)
+
+        # Создаю layout для кнопок
+
+        widget_button_layout = QGridLayout()
+
+        # С помощью сетки размещаем кнопки и запихиваем между кнопками костыли (потом сделаем нормально)
+
+        kostil1 = QLabel(" ")
+        kostil2 = QLabel(" ")
+
+        widget_button_layout.addWidget(button_back, 0, 0)
+        widget_button_layout.addWidget(kostil1, 0, 1)
+        widget_button_layout.addWidget(kostil2, 0, 2)
+        widget_button_layout.addWidget(button_next, 0, 3)
+
+        widget_button.setLayout(widget_button_layout)
+        # Запихиваем вниз кнопки
+        widget_button_layout.setAlignment(Qt.AlignBottom)
+
+
+
+        # Добавляем виджеты в главный виджет
+        main_layout.addWidget(widget_label)
+        main_layout.addWidget(widget_add)
+        main_layout.addWidget(widget_button)
+
+        # Добавляем layout
+        main_widget.setLayout(main_layout)
+
+        # Отображаем главный виджет
+        self.setCentralWidget(main_widget)
+
+
+app = QApplication([])
+window = AddTeacher()
+window.showMaximized()
+app.exec()
