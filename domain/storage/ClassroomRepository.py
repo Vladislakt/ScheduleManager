afc903c12ac5a93bdb30a118313c9ebcb7f93f5b
jhg913915 <tima.trusov@yandex.ru>
2023-11-15 23:11:47 +0300

    Исправил ошибки в методах для работы со списками данных

--- domain/storage/ClassroomRepository.py
+++ domain/storage/ClassroomRepository.py
@@ -1,6 +1,16 @@
+from domain.storage import Classroom
+
+
 class ClassroomRepository:
     def __init__(self):
         self.classroom_list = []
 
-    def add_teacher(self, classroom):
+    def add_classroom(self, classroom: Classroom):
         self.classroom_list.append(classroom)
+
+    def list(self):
+        return list(self.classroom_list.values())
+
+    def fetch(self, classroom_id: int):
+        return self.classroom_list[classroom_id]
+
--- domain/storage/TeacherRepository.py
+++ domain/storage/TeacherRepository.py
@@ -1,6 +1,15 @@
+from domain.storage import Teacher
+
+
 class TeacherRepository:
     def __init__(self):
         self.teacher_list = []
 
-    def add_teacher(self, teacher):
+    def add_teacher(self, teacher: Teacher):
         self.teacher_list.append(teacher)
+
+    def list(self):
+        return list(self.teacher_list.values())
+
+    def fetch(self, teacher_id: int):
+        return self.teacher_list[teacher_id]
