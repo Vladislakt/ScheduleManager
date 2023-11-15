72dbaab7fa796ebff25cbe186cbcc54ab9438575
Artyom-Gorshkov <gorshkov5371@yandex.ru>
2023-11-15 19:12:23 +0300

    Состояние перед подключением к базе данных

--- windows/create_timetable/create_timetable.py
+++ windows/create_timetable/create_timetable.py
@@ -3,15 +3,13 @@ from PySide6.QtWidgets import QWidget, QVBoxLayout
 
 from buttons import Buttons
 from table import Table
-from courses import Courses
-from vertical_line import QVLine
 
 
 class CreateTimetable(QWidget):
     def __init__(self, courses, groups, days, number_of_classes_per_day):
         super().__init__()
         self.setWindowTitle("Составление расписания")
-        self.table = Table(groups, days, number_of_classes_per_day)
+        self.table = Table(courses, groups, days, number_of_classes_per_day)
         self.buttons = Buttons()
         layout = QVBoxLayout()
         layout.addWidget(self.table)
--- windows/create_timetable/table.py
+++ windows/create_timetable/table.py
@@ -4,67 +4,102 @@ from PySide6.QtWidgets import QWidget, QGridLayout, QLabel, QScrollArea, \
 
 from horizontal_line import QHLine
 from vertical_line import QVLine
+from windows.create_timetable.modified_combobox import ModifiedQComboBox
 
 
 class Table(QWidget):
-    def __init__(self, groups, days, number_of_classes_per_day):
+    def __init__(self, courses, groups, days, number_of_classes_per_day):
         super().__init__()
         # self.setStyleSheet("background-color: rgb(255,200,0); ")
+        self.columns_width = 300
+        self.courses = courses
         self.groups = groups
+        self.len_groups = len(self.groups)
         self.days = days
+        self.len_days = len(self.days)
         self.number_of_classes_per_day = number_of_classes_per_day
-        scroll_area = QScrollArea()
-        self.layout = QGridLayout()
-        self.add_days()
-        self.add_numbers_of_classes()
-        self.add_vertical_lines()
-        self.add_horizontal_lines()
-        self.add_groups()
-        widget = QWidget()
-        widget.setLayout(self.layout)
-        scroll_area.setWidget(widget)
+        self.courses_comboboxes = [['' for _ in range(self.len_groups)]
+                                   for _ in range(self.len_days * number_of_classes_per_day)]
+        self.scroll_area_widget = QWidget()
+        self.scroll_area_layout = QGridLayout()
         self.main_layout = QVBoxLayout()
-        self.main_layout.addWidget(scroll_area)
+        self.add_scroll_area()
         self.setLayout(self.main_layout)
 
-    def add_days(self):
-        for i in range(len(self.days)):
-            day = QLabel(self.days[i])
-            self.layout.addWidget(day, 2 + i * (self.number_of_classes_per_day + 1), 0, self.number_of_classes_per_day,
-                                  1)
+    def add_scroll_area(self):
+        scroll_area = QScrollArea()
+        self.create_table()
+        self.scroll_area_widget.setLayout(self.scroll_area_layout)
+        scroll_area.setWidget(self.scroll_area_widget)
+        self.main_layout.addWidget(scroll_area)
 
-    def add_numbers_of_classes(self):
-        for i in range(len(self.days)):
-            for j in range(self.number_of_classes_per_day):
-                label = QLabel(str(j + 1))
-                self.layout.addWidget(label, 2 + i * (self.number_of_classes_per_day + 1) + j, 1, 1, 1)
-
-    def add_groups(self):
-        columns_width = 200
-        for i in range(len(self.groups)):
-            group_name = QLabel(self.groups[i])
-            group_name.setFixedWidth(columns_width)
-            group_name.setAlignment(Qt.AlignCenter)
-            self.layout.addWidget(group_name, 0, 3 + i * 3, 1, 2)
-            for j in range(len(self.days)):
+    def create_table(self):
+        for i in range(self.len_groups):
+            for j in range(self.len_days):
                 for k in range(self.number_of_classes_per_day):
-                    course = QComboBox()
-                    course.addItems(["a", "b", "c"])
-                    course.setFixedWidth(columns_width - 50)
-                    classroom = QComboBox()
-                    classroom.setFixedWidth(50)
-                    self.layout.addWidget(course, 2 + j * (self.number_of_classes_per_day + 1) + k, 3 + i * 3, 1, 1)
-                    self.layout.addWidget(classroom, 2 + j * (self.number_of_classes_per_day + 1) + k, 4 + i * 3, 1, 1)
+                    self.add_days(j)
+                    self.add_number_of_classes(j, k)
+                    self.add_groups(i)
+                    self.add_course_choice(i, j, k)
+                    self.add_classroom_choise(i, j, k)
+                    self.add_horizontal_lines(j)
+                    self.add_vertical_lines(i)
+                    self.connect_to_courses_comboboxes(i, j, k)
 
-    def add_horizontal_lines(self):
-        for i in range(len(self.days)):
-            horizontal_line = QHLine()
-            self.layout.addWidget(horizontal_line, 1 + i * (self.number_of_classes_per_day + 1), 0, 1,
-                                  3 + len(self.groups) * 3)
+    def add_days(self, j):
+        day = QLabel(self.days[j])
+        self.scroll_area_layout.addWidget(day, 2 + j * (self.number_of_classes_per_day + 1), 0,
+                                          self.number_of_classes_per_day, 1)
 
-    def add_vertical_lines(self):
-        for i in range(len(self.groups)):
-            vertical_line = QVLine()
-            vertical_line.setFixedWidth(30)
-            self.layout.addWidget(vertical_line, 0, 2 + i * 3,
-                                  len(self.days) * (self.number_of_classes_per_day + 1) + 1, 1)
+    def add_number_of_classes(self, j, k):
+        label = QLabel(str(k + 1))
+        self.scroll_area_layout.addWidget(label, 2 + j * (self.number_of_classes_per_day + 1) + k, 1, 1, 1)
+
+    def add_groups(self, i):
+        group_name = QLabel(self.groups[i])
+        group_name.setFixedWidth(self.columns_width)
+        group_name.setAlignment(Qt.AlignCenter)
+        self.scroll_area_layout.addWidget(group_name, 0, 3 + i * 3, 1, 2)
+
+    def add_course_choice(self, i, j, k):
+        course = ModifiedQComboBox(2 + j * (self.number_of_classes_per_day + 1) + k, 3 + i * 3, self.scroll_area_widget)
+        course.addItem("")
+        course.addItems(self.courses)
+        course.view().setMinimumWidth(300)
+        course.setFixedWidth(self.columns_width - 50)
+        self.scroll_area_layout.addWidget(course, 2 + j * (self.number_of_classes_per_day + 1) + k,
+                                          3 + i * 3, 1, 1)
+
+    def connect_to_courses_comboboxes(self, i, j, k):
+        course_combobox = self.scroll_area_layout.itemAtPosition(2 + j * (self.number_of_classes_per_day + 1) + k,
+                                                                 3 + i * 3).widget()
+        course_combobox.currentTextChanged.connect(lambda: self.check_lessons_in_row(course_combobox.get_row()))
+
+    def check_lessons_in_row(self, row):
+        teacher_ids = []
+        for i in range(self.len_groups):
+            current_combobox_item_index = self.scroll_area_layout.itemAtPosition(row, 3 + i * 3).widget().currentIndex()
+            
+            if id in teacher_ids:
+                teacher_ids.append(id)
+                teacher_ids.index(id)
+                self.scroll_area_layout.itemAtPosition(row, 3 + i * 3). \
+                    widget().setStyleSheet("background-color: rgb(255,255,255); ")
+            else:
+                teacher_ids.append(id)
+
+    def add_classroom_choise(self, i, j, k):
+        classroom = QComboBox()
+        classroom.setFixedWidth(50)
+        self.scroll_area_layout.addWidget(classroom, 2 + j * (self.number_of_classes_per_day + 1) + k,
+                                          4 + i * 3, 1, 1)
+
+    def add_horizontal_lines(self, j):
+        horizontal_line = QHLine()
+        self.scroll_area_layout.addWidget(horizontal_line, 1 + j * (self.number_of_classes_per_day + 1), 0, 1,
+                                          3 + self.len_groups * 3)
+
+    def add_vertical_lines(self, i):
+        vertical_line = QVLine()
+        self.scroll_area_layout.addWidget(vertical_line, 0, 2 + i * 3,
+                                          self.len_days * (self.number_of_classes_per_day + 1) + 1, 1)
