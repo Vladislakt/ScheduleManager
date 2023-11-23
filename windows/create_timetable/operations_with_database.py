19a27cad517e506b8ce460b018958d7db4e9ac08
Artyom-Gorshkov <gorshkov5371@yandex.ru>
2023-11-23 19:27:10 +0300

    Оптимизация работы с БД

--- windows/create_timetable/operations_with_database.py
+++ windows/create_timetable/operations_with_database.py
@@ -3,31 +3,39 @@ from DataBase.select_queries import getTeacherName
 
 db = "test"
 
-group_names = getGroupNameList(db)
-len_group_names = len(group_names)
 groups = getGroupList(db)
 classrooms = getClassroomList(db)
 
 
-def get_courses_with_teachers():
-    courses = []
-    for group in group_names:
-        lessons = getLessonsByGroup(db, group)
-        courses_for_one_group = []
-        for i in range(len(lessons)):
-            courses_for_one_group.append(f"{lessons[i].lesson_name} ({getTeacherName(db, lessons[i].teach_id)})")
-        courses.append(courses_for_one_group)
-    return courses
+def get_group_names():
+    names = []
+    for group in groups:
+        names.append(group.group_name)
+    return names
 
 
-courses_with_teachers = get_courses_with_teachers()
+group_names = get_group_names()
+len_group_names = len(group_names)
 
 
 def get_courses():
     courses_for_all_groups = []
-    for i in range(len(group_names)):
-        courses_for_all_groups.append(getLessonsByGroup(db, group_names[i]))
+    for group in group_names:
+        courses_for_all_groups.append(getLessonsByGroup(db, group))
     return courses_for_all_groups
 
 
 courses = get_courses()
+
+
+def get_courses_with_teachers():
+    courses_teachers = []
+    for i in range(len(courses)):
+        one_group = []
+        for j in range(len(courses[i])):
+            one_group.append(f"{courses[i][j].lesson_name} ({getTeacherName(db, courses[i][j].teach_id)})")
+        courses_teachers.append(one_group)
+    return courses_teachers
+
+
+courses_with_teachers = get_courses_with_teachers()
