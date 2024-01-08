95bc27f8a7030501a448f9ede9a802eef1e18eb1
Vladislakt <vlad1.999vlad@mail.ru>
2024-01-08 15:42:34 +0300

    Добавление функции проверки ввода кабинетов Исправленны другие функции проверки

--- windows/pre_timetable_windows/window_data_check.py
+++ windows/pre_timetable_windows/window_data_check.py
@@ -3,14 +3,21 @@ def check_new_bd_name(new_name):
 
 
 def check_teachers_data(teacher_list):
-    for line in teacher_list:
-        if line.teacher.text() == "":
+    for index in range(1, len(teacher_list)):
+        if teacher_list[index].teacher.text() == "":
             return False
     return True
 
 
 def check_groups_data(group_list):
-    for line in group_list:
-        if line.group.text() == "" or line.size.text() == "":
+    for index in range(1, len(group_list)):
+        if group_list[index].group.text() == "" or group_list[index].size.text() == "":
+            return False
+    return True
+
+
+def check_classrooms_data(classroom_list):
+    for index in range(1, len(classroom_list)):
+        if classroom_list[index].classroom.text() == "" or classroom_list[index].size.text() == "" or classroom_list[index].computer.text() == "":
             return False
     return True
