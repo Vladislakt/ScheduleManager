5265ee8bd30e21eba68ad66b7fff0d8cc4924baf
Vladislakt <vlad1.999vlad@mail.ru>
2024-01-08 15:35:52 +0300

    Добавление функции проверки ввода групп

--- windows/pre_timetable_windows/window_data_check.py
+++ windows/pre_timetable_windows/window_data_check.py
@@ -7,3 +7,10 @@ def check_teachers_data(teacher_list):
         if line.teacher.text() == "":
             return False
     return True
+
+
+def check_groups_data(group_list):
+    for line in group_list:
+        if line.group.text() == "" or line.size.text() == "":
+            return False
+    return True
