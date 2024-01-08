221c6e658f443318c828e0ae37c6f33ec27cce3b
Vladislakt <vlad1.999vlad@mail.ru>
2024-01-08 15:56:40 +0300

    Исправленны функции проверки

--- windows/pre_timetable_windows/window_data_check.py
+++ windows/pre_timetable_windows/window_data_check.py
@@ -3,6 +3,8 @@ def check_new_bd_name(new_name):
 
 
 def check_teachers_data(teacher_list):
+    if len(teacher_list) == 1:
+        return False
     for index in range(1, len(teacher_list)):
         if teacher_list[index].teacher.text() == "":
             return False
@@ -10,6 +12,8 @@ def check_teachers_data(teacher_list):
 
 
 def check_groups_data(group_list):
+    if len(group_list) == 1:
+        return False
     for index in range(1, len(group_list)):
         if group_list[index].group.text() == "" or group_list[index].size.text() == "":
             return False
@@ -17,6 +21,8 @@ def check_groups_data(group_list):
 
 
 def check_classrooms_data(classroom_list):
+    if len(classroom_list) == 1:
+        return False
     for index in range(1, len(classroom_list)):
         if classroom_list[index].classroom.text() == "" or classroom_list[index].size.text() == "" or classroom_list[index].computer.text() == "":
             return False
