851cc800e31026e57393b337fdbad91c39d78877
Vladislakt <vlad1.999vlad@mail.ru>
2024-01-08 15:32:43 +0300

    Добавление функции проверки ввода преподавателей

--- windows/pre_timetable_windows/window_data_check.py
+++ windows/pre_timetable_windows/window_data_check.py
@@ -1,2 +1,9 @@
 def check_new_bd_name(new_name):
     return new_name != ""
+
+
+def check_teachers_data(teacher_list):
+    for line in teacher_list:
+        if line.teacher.text() == "":
+            return False
+    return True
